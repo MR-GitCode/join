1b109c1c5850576f03357a85fd20ef78641825d3
EricWinzer <Eric@Winzer.org>
2025-03-15 12:24:48 +0100

    fixed DB 'users' and added DB 'tasks'

--- scripts/db.js
+++ scripts/db.js
@@ -1,26 +1,42 @@
-const BASE_URL = 'https://join-441-default-rtdb.europe-west1.firebasedatabase.app/';
+const BASE_URL = 'https://join-441-default-rtdb.europe-west1.firebasedatabase.app/join/';
 
-/* path = '/name' */
-async function loadData(path = '') {
-    let response = await fetch(BASE_URL + path + '.json');
-    return responseToJson = await response.json();
+async function loadData() {
+    try {
+        let usersData = await fetch(`${BASE_URL}/users.json`);
+        users = (await usersData.json()) || [];
+
+        let tasksData = await fetch(`${BASE_URL}/tasks.json`);
+        tasks = (await tasksData.json()) || [];
+    } catch (error) {
+        console.error('Fehler beim Laden der Daten:', error);
+    }
 };
 
-async function postData(path = '', data = {}) {
-    let response = await fetch(BASE_URL + path + '.json', {
-        method: 'POST',
-        header: {
+/* type = 'users' || 'tasks' || '' (alle Daten) und data = null (alle Daten) oder id */
+function saveData(type = '', data = null) {
+    if (data) {
+        transmitData(type, data);
+    } else {
+        users.forEach(user => transmitData('users', user));
+        tasks.forEach(task => transmitData('tasks', task));
+    }
+};
+
+async function transmitData(path = '', data = {}) {
+    let response = await fetch(`${BASE_URL}/${path}/${data.id}.json`, {
+        method: 'PUT',
+        headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data)
     });
-    return responseToJson = await response.json();
+    return await response.json();
 };
 
-async function deleteData(path = '') {
-    let response = await fetch(BASE_URL + path + '.json', {
+async function deleteData(path = '', id) {
+    let response = await fetch(`${BASE_URL}/${path}/${id}.json`, {
         method: 'DELETE',
     });
-    return responseToJson = await response.json();
+    return await response.json();
 };
 
