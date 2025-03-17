e30e55c48080c451bb478c9f7acb11b07f4a41d5
Michael Ring <michael.ring.87@web.de>
2025-03-17 12:22:11 +0100

    add drop down menu for input field category

--- scripts/add_task.js
+++ scripts/add_task.js
@@ -1,5 +1,5 @@
 /**
- * This function is used to change the backgroundcolor of die piority buttons.
+ * This function is used to change the backgroundcolor of the piority buttons.
  * @param {string} piority
  */
 function selectPiority(piority) {
@@ -8,3 +8,21 @@ function selectPiority(piority) {
     document.getElementById('bt-low').classList.remove('bt-low');
     document.getElementById(`bt-${piority}`).classList.add(`bt-${piority}`);
 } 
+
+/**
+ * This function open the drop down menu for the category input field.
+ * 
+ */
+function openCategoryMenu() {
+    let categories = document.getElementById("categories");
+    categories.style.display = categories.style.display === "block" ? "none" : "block";
+}
+
+/**
+ * This function changes the category in the input field.
+ * @param {string} category Is the select category.
+ */
+function selectCategory(category) {
+    document.getElementById('category-input').value = category;
+    document.getElementById("categories").style.display = "none";
+}
