a7c0f71fdc0a5fccc9c73c691881dd93e1235e81
Michael Ring <michael.ring.87@web.de>
2025-04-15 19:36:11 +0200

    add eventlistener close overlay outside

--- scripts/board.js
+++ scripts/board.js
@@ -1,9 +1,25 @@
-function closeOverlay() {
-    let overlayContainer = document.getElementById("overlay-add-task")
-    overlayContainer.classList.add('hidden');
-}
-
+/**
+ * Opens the "Add Task" overlay by removing the 'hidden' class.
+ * Triggered when the "Add Task" button is clicked.
+ */
 function openOverlay() {
     let overlayContainer = document.getElementById("overlay-add-task")
     overlayContainer.classList.remove('hidden');
+    document.body.classList.add('no-scroll');
 }
+
+/**
+ * Closes the "Add Task" overlay by adding the 'hidden' class.
+ */
+function closeOverlay() {
+    let overlayContainer = document.getElementById("overlay-add-task")
+    overlayContainer.classList.add('hidden');
+    document.body.classList.remove('no-scroll');
+}
+
+document.getElementById("overlay-add-task").addEventListener("click", function (event) {
+    let overlayContainer = document.querySelector(".content-add-task");
+    if (!overlayContainer.contains(event.target)) {
+        closeOverlay();
+    }
+});
